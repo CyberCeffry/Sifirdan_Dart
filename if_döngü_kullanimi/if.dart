551d9be8ce2171530c2500e65512236ab4841656
CyberCeffry <bcirit76@gmail.com>
2026-09-10 17:25:14 +0300

    ifs

---
+++ if_döngü_kullanimi/if.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String ad = "Burak", soyAd = "burak";
+
+  if (ad == soyAd) {
+    print("Ad ve soyad eşit");
+  } else {
+    print("Ad ve soyad eşit değil");
+  }
+}
