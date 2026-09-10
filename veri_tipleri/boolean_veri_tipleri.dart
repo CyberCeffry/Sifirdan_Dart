1dc0534c2ca371d1e74a5fb272eafcd4420bbecf
CyberCeffry <bcirit76@gmail.com>
2026-09-10 14:50:46 +0300

    devam2

---
+++ veri_tipleri/boolean_veri_tipleri.dart
@@ -0,0 +1,14 @@
+void main() {
+  bool ogrenciMi = false;
+  print(ogrenciMi);
+
+  bool evliMi = true;
+  print(evliMi);
+
+  var iyiMi = false;
+  if (iyiMi == true) {
+    print("İyi");
+  } else {
+    print("Kötü");
+  }
+}
