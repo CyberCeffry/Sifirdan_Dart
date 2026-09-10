db172d76b705996c2e346d0e30d088d8c30cae60
CyberCeffry <bcirit76@gmail.com>
2026-09-10 14:37:40 +0300

    devam

---
+++ veri_tipleri/number_veri_tip.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  int yas = 31;
+
+  print(yas);
+
+  yas = yas + 15;
+
+  print(yas);
+
+  double sicaklik = 50.15;
+
+  print(sicaklik);
+
+  sicaklik = sicaklik - 10.15;
+
+  print(sicaklik);
+
+  num sayi = 1;
+
+  print(sayi);
+
+  sayi = 1.4;
+
+  print(sayi);
+}
