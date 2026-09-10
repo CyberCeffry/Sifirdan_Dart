315d96a0f82ab4114ae6858d61ee6556b1345802
CyberCeffry <bcirit76@gmail.com>
2026-09-10 15:10:03 +0300

    vars

--- veri_tipleri/string_veri_tipi.dart
+++ veri_tipleri/string_veri_tipi.dart
@@ -1,1 +1,15 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String ad = "Burak";
+  String soyAd = "Cirit";
+  int yas = 31;
+  bool ogrenciMi = false;
+  print(ad);
+  print(soyAd);
+  print(yas);
+  print(ogrenciMi);
+  print(
+    "Benim adım $ad $soyAd ve ben $yas yaşındayım. Öğrenci miyim? $ogrenciMi",
+  );
+  var tamAd = ad + " " + soyAd;
+  print(tamAd.length);
+}
